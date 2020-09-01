cb9c7396236ec45ff14df65a24130e456b8fe045
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2020-09-01 16:00:22 +0900

    mainの更新

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
-#include<Windows.h>
-#include<fstream>
-#include"Core.h"
+#define ENGINE_DIRECTX11
 
-#include"Core/IO/FileStream.h"
+#include <stdlib.h>  
+#include <crtdbg.h> 
+
+#include<string>
+
+#include"Platform.h"
+#include"Core.h"
+#include"Engine.h"
+
+#include"Core/IO/Path.h"
 
 int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
 {
+	TCHAR curDir[MAX_PATH + 1];
+	GetCurrentDirectory(MAX_PATH + 1,curDir);
+
+	Core::Path path(curDir);
+	auto file = path.getFile();
+
+	Platform::Windows::Window window;
+	window.Create(
+		hInstance,
+		(LPSTR)"MainWindow",
+		(LPSTR)"MainWindow",
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		1024,576,
+		WS_OVERLAPPEDWINDOW
+	);
+
+	Platform::Graphics::DX11Graphics::Create();
+
+	Engine::System mainSystem;
+	mainSystem.m_Renderer = new Platform::Graphics::DX11Renderer(window.GetHWnd());
+	mainSystem.mainLoop();
+
+	Platform::Graphics::DX11Graphics::Destroy();
+	
 	return 0;
 }
