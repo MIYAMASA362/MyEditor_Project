90975ddd847b59d6f79bf294a53ffa5ca9228a3b
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2020-11-17 15:46:17 +0900

    add : Taskヘッダー

---
+++ include/Core/Task/Task.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef CORE_TASK_H
+#define CORE_TASK_H
+
+namespace Core
+{
+	/**
+	* @brief    Task of State
+	*/
+	enum class TaskState
+	{
+		None,
+		Running,
+		Finished,
+		Canceled,
+	};
+
+}// namespace Core
+
+namespace Core
+{
+	namespace detail
+	{
+		/**
+		* @class    ITask
+		* @brief    Task Interface
+		*/
+		class ITask
+		{
+		public:
+			virtual void run() = 0;
+			virtual void cancel() = 0;
+
+			virtual TaskState GetState() const;
+		protected:
+			ITask();
+			virtual ~ITask();
+
+			TaskState m_TaskState;
+
+		};// class ITask
+
+	}// namespace detail
+}// namespace Core
+
+
+#endif // ifndef CORE_TASK_H
---
+++ src/Core/Task/Task.cpp
@@ -0,0 +1,23 @@
+#include"stdafx.h"
+#include"Core/Task/Task.h"
+
+namespace Core
+{
+	TaskState detail::ITask::GetState() const
+	{
+		return m_TaskState;
+	}
+
+	detail::ITask::ITask()
+		:
+		m_TaskState(TaskState::None)
+	{
+
+	}
+
+	detail::ITask::~ITask()
+	{
+
+	}
+
+}// namespace Core
